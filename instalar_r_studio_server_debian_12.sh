40737babadbd3fd48e7edceb09f148cdb05354e2
Luiz Carlos P. C. Mendes <61766359+lcpcmfafire@users.noreply.github.com>
2024-03-24 19:56:41 -0300

    instalar_r_studio_server_debian_12.sh
    teste resumido instalar R

--- instalar_r_studio_server_debian_12.sh
+++ instalar_r_studio_server_debian_12.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-function atualiza_repositorio(){
-
-echo ""
-echo "
-deb http://deb.debian.org/debian bookworm main non-free-firmware
-deb-src http://deb.debian.org/debian bookworm main non-free-firmware
-
-deb http://deb.debian.org/debian-security/ bookworm-security main non-free-firmware
-deb-src http://deb.debian.org/debian-security/ bookworm-security main non-free-firmware
-
-deb http://deb.debian.org/debian bookworm-updates main non-free-firmware
-deb-src http://deb.debian.org/debian bookworm-updates main non-free-firmware" > /etc/apt/sources.list
-
-apt update 
-apt upgrade -y
-
-}
-
-
 function instalar_requisitos(){
 
-apt install -y sudo gdebi-core wget apt-transport-https ca-certificates curl gnupg2 
+apt install -y gdebi-core
 
 }
 
@@ -39,7 +20,7 @@ cd /tmp
 
 wget wget https://download2.rstudio.org/server/jammy/amd64/rstudio-server-2023.12.1-402-amd64.deb
 
-sudo gdebi rstudio-server-2023.12.1-402-amd64.deb -y
+gdebi --non-interactive rstudio-server-2023.12.1-402-amd64.deb
 
 }
 
@@ -54,8 +35,6 @@ echo "http://$ip:8787"
 
 }
 
-
-atualiza_repositorio
 instalar_requisitos
 instala_r_base
 download_rstudio_server
